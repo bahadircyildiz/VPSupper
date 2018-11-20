5c59f1559cede63cc77ac8a9bf4d2cb3f92e76e8
Bahadir Can Yildiz <bahadircyildiz@yandex.com>
2018-11-20 07:30:05 +0100

    syntax changes

--- src/setup_git-aware-prompt.sh
+++ src/setup_git-aware-prompt.sh
@@ -4,8 +4,8 @@ echo "Setting up git-aware-prompt..."
 mkdir ~/.bash
 cd ~/.bash
 git clone git://github.com/jimeh/git-aware-prompt.git
-echo 'export GITAWAREPROMPT=~/.bash/git-aware-prompt' >>~ ~/.bashrc
-echo 'source "${GITAWAREPROMPT}/main.sh\"' >> ~/.bashrc
+echo 'export GITAWAREPROMPT=~/.bash/git-aware-prompt' >> ~/.bashrc
+echo 'source ${GITAWAREPROMPT}/main".sh' >> ~/.bashrc
 echo 'export PS1="\${debian_chroot:+(\$debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\] \[$txtcyn\]\$git_branch\[$txtred\]\$git_dirty\[$txtrst\]\$ "' >> ~/.bashrc
 cd ~
 source ~/.bashrc
